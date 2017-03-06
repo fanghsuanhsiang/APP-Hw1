5e3f6e9d102f70e589209af7a01497d2afb6bba3
fanghsuanhsiang <fanghsuanhsiang@gmail.com>
2017-03-06 14:29:34 +0800

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,10 @@
+var greetMe = function() {
+	console.log('Hi Hsiang!');
+}
+greetMe();
+
+greetsponge = () => console.log('Hi 海綿!');
+greetsponge();
+
+greetYou = name => console.log('Hi ' + name + '!');
+greetYou('NTUE');
